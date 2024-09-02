9e4c13f5243906632ca4a18a5fc9b04ea4b77348
inakalpaka <67813256+inakalpaka@users.noreply.github.com>
2024-09-02 16:43:59 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -17,51 +17,20 @@ mongoose.connect(uri)
   .then(() => console.log('Conectado a MongoDB'))
   .catch(err => console.error('Error al conectar a MongoDB', err));
 
-app.post('/api/login', async (req, res) => {
+app.post('/api/iniciarSesion', async (req, res) => {
   const { nombre, contraseña } = req.body;
-
-  //comprobar si existe el usuario
-  //comparar la contraseña con la contraseña hasheada
-
-
   try {
-    res.status(201).send('Sesion iniciada');
-  } catch (error) {
-    console.error('Error iniciando sesion', error);
-    res.status(500).send('Error iniciar sesion');
-  }
-  /*
-  Ejemplo de JSON para prueba:
-  {
-    "nombre": "Juansito",
-    "contraseña": "qwerty12345"
-  }
-  */
-});
-app.get('/api/contactos', async (req, res) => {
-  try {
-    const usuarios = await Usuario.find();
-    res.json(usuarios);
+    const elUsuario = await Usuario.findOne({ nombre: nombre });
   } catch (error) {
     console.error('Error al obtener contactos:', error);
     res.status(500).send('Error al obtener contactos');
   }
-  const { nombre, contraseña } = req.body;
-
-  bcrypt.compare(contraseña, hash, function(err, result) {
-    // result == true
-});
-    const nuevoUsuario = new Usuario({
-      nombre,
-      hash
-    });
-
-    try {
-      await nuevoUsuario.save();
-      res.status(201).send('Contacto agregado exitosamente');
-    } catch (error) {
-      console.error('Error al agregar contacto:', error);
-      res.status(500).send('Error al agregar contacto');
+  bcrypt.compare(contraseña, elUsuario.contraseña, function (err, result) {
+    if (result == true) {
+      res.status(201).send('sesion iniciada');
+    } else {
+      console.error('Error al iniciar sesion, la contraseña no coiside');
+      res.status(500).send('Error al iniciar sesion');
     }
   });
 });
@@ -92,21 +61,6 @@ app.post('/api/registrarse', async (req, res) => {
     "contraseña": "qwerty12345"
   }
   */
-});
-
-app.delete('/api/contactos/:telefono', async (req, res) => {
-  try {
-    const resultado = await Contacto.findOneAndDelete({ telefono: req.params.telefono });
-
-    if (!resultado) {
-      return res.status(404).send('Contacto no encontrado');
-    }
-
-    res.status(200).send('Contacto eliminado exitosamente');
-  } catch (error) {
-    console.error('Error al eliminar contacto:', error);
-    res.status(500).send('Error al eliminar contacto');
-  }
 });
 
 app.listen(PORT, () => {
